0e6bfd8b5f7bca7a89595bd0f99d5b15cb52aaf3
mishashto <me@reze.moe>
2025-05-22 11:15:32 +0900

    chore: display amount of users on index

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const userAmount = ref(0)
+</script>
 <template>
     <div class="main-page-content">
         <h1>Really Cute File Uploader</h1>
-        <p>Only way I know how to upload files now</p>
+        <p>Only way {{ userAmount }} people know how to upload files now</p>
     </div>
 </template>
