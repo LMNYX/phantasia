7c7b42ff2b1c224fcf6fff523eab7acdbea9f085
mishashto <me@reze.moe>
2025-05-23 18:30:39 +0900

    feat: add @nuxt/charts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: [
@@ -16,12 +18,12 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-
   modules: [
     '@nuxt/image',
     '@nuxt/fonts',
     '@nuxt/icon',
-    '@nuxt/scripts'
+    '@nuxt/scripts',
+    'nuxt-charts'
   ],
   fonts:
   {
