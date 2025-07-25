eb53a8fe3b4a81f41e7f0c887591f7fe3347909e
LMNYX <me@reze.moe>
2025-07-25 16:51:17 +0500

    chore(api): user model id needs to be unique generated pk

--- api/src/models/user.py
+++ api/src/models/user.py
@@ -1,8 +1,9 @@
+import hashlib
 from tortoise import fields
 from tortoise.models import Model
 
 class User(Model):
-    id = fields.IntField(pk=True)
+    id = fields.IntField(pk=True, generated=True, unique=True, null=False)
     username = fields.CharField(max_length=255, unique=True)
     access_key = fields.CharField(max_length=255)
     permissions = fields.JSONField(default=['USER_ALL'])
@@ -12,6 +13,15 @@ class User(Model):
     updated_at = fields.DatetimeField(auto_now=True)
     views = fields.IntField(default=0)
     uploads = fields.IntField(default=0)
+    twitch_user_id = fields.CharField(max_length=255, null=True)
+
+    def encrypt_access_key(self):
+        sha_signature = hashlib.sha256(self.access_key.encode()).hexdigest()
+        return sha_signature
+    
+    def compare_access_key(self, access_key):
+        sha_signature = hashlib.sha256(access_key.encode()).hexdigest()
+        return sha_signature == self.access_key
 
     class Meta:
         table = "users"
