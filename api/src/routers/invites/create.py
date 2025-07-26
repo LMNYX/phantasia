48466b93c371fde3ef4e742b676ee63e8af6dba3
LMNYX <me@reze.moe>
2025-07-26 14:44:04 +0500

    feat(api): implement `/invites/create` route

---
+++ api/src/routers/invites/create.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, Response, status
+from pydantic import BaseModel
+from typing import List
+
+from src.models.user import User
+from src.models.invite import Invite
+from src.internal.authmgr import requires_auth
+from src.internal.error import ErrorDetails
+from src.utils.random import generate_random_string
+
+router = APIRouter(prefix="/invites", tags=["Invites"])
+
+class NewInvitationCreated(BaseModel):
+    inviation_code: str
+
+@router.get("/create", status_code=201,
+            summary="Creates an invitation code for a new user",
+            description="Creates an invitation code for a new user",
+            responses={
+                status.HTTP_200_OK: {"model": NewInvitationCreated},
+                status.HTTP_400_BAD_REQUEST: {"model": ErrorDetails}
+            })
+@requires_auth(permissions_required=["INVITE_CREATE"])
+async def create_new_invite(username: str, response: Response, *,
+    user: User):
+    
+    if await User.exists_by_username(username):
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return ErrorDetails(error=f"User already exists with username provided", status_code=status.HTTP_400_BAD_REQUEST)
+
+    inviation_code = generate_random_string(16)
+
+    new_invite = await Invite.create(
+        username=username,
+        inviter=user,
+        inviation_code=inviation_code
+    )
+
+    return NewInvitationCreated(inviation_code=inviation_code)
---
+++ api/src/utils/random.py
@@ -0,0 +1,8 @@
+import random
+
+def generate_random_string(length: int):
+    return "".join(
+        random.choices(
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", k=length
+        )
+    )
